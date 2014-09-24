ef56f9e8910089a96fb658b054beb6ac62441292
breaklinkability <break.linkability@gmail.com>
2014-09-24 15:47:09 +0200

    Canvas fingerprinting added to the JavaScript fingerprinting script

--- FPScripts/fp.js
+++ FPScripts/fp.js
@@ -35,6 +35,21 @@ if (window.navigator.plugins) {
 	  plugins+= "Plugin "+i+": " + plist[i];
 }
 
+//Canvas fingerprinting
+canvas = document.createElement("canvas");
+canvasContext = canvas.getContext("2d");
+canvas.style.display = "inline";
+canvasContext.textBaseline = "alphabetic";
+canvasContext.fillStyle = "#f60";
+canvasContext.fillRect(125, 1, 62, 20);
+canvasContext.fillStyle = "#069";
+canvasContext.font = "11pt no-real-font-123";
+canvasContext.fillText("Cwm fjordbank glyphs vext quiz, \ud83d\ude03",2,15);
+canvasContext.fillStyle = "rgba(102, 204, 0, 0.7)";
+canvasContext.font =  "18pt Arial";
+canvasContext.fillText("Cwm fjordbank glyphs vext quiz, \ud83d\ude03",4,17);
+canvasData = canvas.toDataURL();
+
 try {
 	ieUserData = "";
     oPersistDiv.setAttribute("testStorage", "value remembered");
